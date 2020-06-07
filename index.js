e3d37a84f0fb3d474e31b6b7c441d0fc1d87fe1d
Louis <colletlouis@gmail.com>
2020-06-07 16:42:13 +0200

    Fixed routes

--- index.js
+++ index.js
@@ -1,67 +1,70 @@
 const dotenv = require('dotenv');
+dotenv.config();
+const connection = require('./conf');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-
 app.use(express.json());
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // appel des photos disponible
 
 app.get('/api/pics', (req, res) => {
-    connection.query('SELECT * FROM ', (err, results) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('something bad happenned')
-        } else {
-            if (!results.length) {
-                res.status(404).send('Pas de photos disponible')
-            } else {
-                res.status(200).json(results);
-            }
-        }
-    })
-})
+  connection.query('SELECT * FROM base_pic', (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('something bad happenned');
+    } else {
+      if (!results.length) {
+        res.status(404).send('Pas de photos disponible');
+      } else {
+        res.status(200).json(results);
+      }
+    }
+  });
+});
 
 // appel des memes enregistrés
 
-app.get('/api/meme', (req, res) => {
-    connection.query('SELECT * FROM meme', (err, results) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('something bad happenned')
-        } else {
-            if (!results.length) {
-                res.status(404).send('Pas de meme disponible')
-            } else {
-                res.status(200).json(results);
-            }
-        }
-    })
-})
+app.get('/api/memes', (req, res) => {
+  connection.query('SELECT * FROM meme', (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('something bad happenned');
+    } else {
+      if (!results.length) {
+        res.status(404).send('Pas de meme disponible');
+      } else {
+        res.status(200).json(results);
+      }
+    }
+  });
+});
 
 // poster un meme
 
-app.post('/api/meme', (req, res) => {
-    const text = req.body;
-    connection.query('INSERT INTO meme SET ?', [text], (err, results) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Problème lors de la sauvegarde')
-        } else {
-            res.status(200).send('enregistrement effectué')
-        }
-    })
-})
+app.post('/api/memes', (req, res) => {
+  const text = req.body;
+  connection.query('INSERT INTO meme SET ?', [text], (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Problème lors de la sauvegarde');
+    } else {
+      res.status(200).send('Enregistrement effectué');
+    }
+  });
+});
 
 app.listen(port, (err) => {
-    if (err) {
-        throw new Error('Something bad happened...');
-    }
+  if (err) {
+    throw new Error('Something bad happened...');
+  }
 
-    console.log(`Server is listening on ${port}`);
+  console.log(`Server is listening on ${port}`);
 });
